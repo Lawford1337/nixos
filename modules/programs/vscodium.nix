87a1beca2d977e50f84863bb26ee946e83cf5d5c
Ilya <alexander35480@gmail.com>
2026-05-23 22:54:09 +0500

    bugfix

--- modules/programs/vscodium.nix
+++ modules/programs/vscodium.nix
@@ -13,7 +13,7 @@ in
       programs.vscodium = {
         enable = true;
 	package = pkgs.vscodium;
-
+        profiles.default = {
 	extensions = with pkgs.vscode-extensions; [
 	  bbenoist.nix
 	  rust-lang.rust-analyzer
@@ -30,7 +30,8 @@ in
 	  "editor.fontSize" = 14;
 	  "editor.formatOnSave" = true;
 	  "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'Courier New', monospace";
-	};
+	 };
+	}:
       };
     };
   };
