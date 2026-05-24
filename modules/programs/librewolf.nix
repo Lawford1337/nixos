323b266c0b6da50fbdeb79aa0e600baf2b32848a
Ilya <alexander35480@gmail.com>
2026-05-25 00:05:42 +0500

    fix librewolf

--- modules/programs/librewolf.nix
+++ modules/programs/librewolf.nix
@@ -20,11 +20,17 @@ in
           "privacy.clearOnShutdown.cookies" = false;
           "privacy.clearOnShutdown.history" = false;
           "privacy.clearOnShutdown.offlineApps" = false;
-	  "privacy.resistFingerprinting" = false;
+          "privacy.resistFingerprinting" = false;
           "network.cookie.lifetimePolicy" = 0;
           "privacy.userContext.enabled" = true;
           "privacy.userContext.ui.enabled" = true;
-	  "webgl.disabled" = false;
+          "webgl.disabled" = false;
+          
+          "privacy.sanitize.sanitizeOnShutdown" = false;
+          
+          "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+          "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = false;
+          "privacy.clearOnShutdown_v2.siteSettings" = false;
         };
       };
     };
