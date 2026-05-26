6bb53318ff5f64e5503ca05ce672077218413309
Ilya <alexander35480@gmail.com>
2026-05-26 12:44:25 +0500

    addon

--- modules/programs/init.lua
+++ modules/programs/init.lua
@@ -103,4 +103,34 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", 
+      "hrsh7th/cmp-path",    
+      "hrsh7th/cmp-buffer", 
+      "L3MON4D3/LuaSnip",     
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args) require("luasnip").lsp_expand(args.body) end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(), 
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(), 
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" }, 
+          { name = "path" },     
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+    end,
+  },
 })
