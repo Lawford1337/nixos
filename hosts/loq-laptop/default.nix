8b436d925c921f5d29cef5464012338191d24f77
Ilya <alexander35480@gmail.com>
2026-05-26 08:56:29 +0500

    enable nvim.nix

--- hosts/loq-laptop/default.nix
+++ hosts/loq-laptop/default.nix
@@ -33,7 +33,7 @@
   lawford.desktop.theme.enable = true;
   lawford.desktop.waybar.enable = true;
   lawford.desktop.launcher.enable = true;
-
+  lawford.programs.nvim.enable = true;
   users.users.lawford = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "audio" "docker" ];
