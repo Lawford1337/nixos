4dc4fd13dc6a60829db29c88f1ba0bdd225314f2
Ilya <alexander35480@gmail.com>
2026-06-08 13:02:43 +0500

    settings nvidia (desktop host )

---
+++ modules/hardware/desktop-nvidia.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  hardware.nvidia = {
+    modesetting.enable = true;
+    
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    
+    open = false;
+    
+    nvidiaSettings = true;
+    
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.cpu.intel.updateMicrocode = true;
+
+  services.fstrim.enable = true;
+}
