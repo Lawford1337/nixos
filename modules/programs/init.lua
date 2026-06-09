4ccf7c52ca8657a1637618ecb1bc6bb98c8c7626
Ilya <alexander35480@gmail.com>
2026-06-09 18:19:34 +0500

    add hot keys

--- modules/programs/init.lua
+++ modules/programs/init.lua
@@ -158,7 +158,12 @@ require("lazy").setup({
       "onsails/lspkind.nvim",
     },
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
