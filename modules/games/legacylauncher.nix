23af3bc86326ed71d11a61cf69e879ebaa6e81b2
Ilya <alexander35480@gmail.com>
2026-06-13 04:04:25 +0500

    bugfix legacy launcher

--- modules/games/legacylauncher.nix
+++ modules/games/legacylauncher.nix
@@ -8,9 +8,24 @@ let
     hash = "sha256-Cbp1F8ipsweA/5pt4jC4kFJHg1rg2pFNZpkeKztLbE4=";
   };
 
-  legacylauncher = pkgs.writeShellScriptBin "legacylauncher" ''
-    exec ${pkgs.jdk17}/bin/java -jar ${bootstrap} "$@"
-  '';
+  legacylauncher = pkgs.symlinkJoin {
+    name = "legacylauncher";
+    paths = [
+      (pkgs.writeShellScriptBin "legacylauncher" ''
+        export JAVA_HOME="${pkgs.jdk17}"
+        export PATH="${pkgs.jdk17}/bin:$PATH"
+        export _JAVA_OPTIONS="-Djava.net.useSystemProxies=true"
+        exec ${pkgs.jdk17}/bin/java -jar ${bootstrap} "$@"
+      '')
+      (pkgs.makeDesktopItem {
+        name = "legacylauncher";
+        desktopName = "Legacy Launcher";
+        exec = "legacylauncher";
+        icon = "legacylauncher";
+        categories = [ "Game" ];
+      })
+    ];
+  };
 in
 {
   options.lawford.games.legacylauncher = {
