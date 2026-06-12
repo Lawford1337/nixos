99e3ac2dd0722c8ccc224cdd576db2b9d6bb8631
Ilya <alexander35480@gmail.com>
2026-06-12 12:25:00 +0500

    add pipewire

--- hosts/desktop-pc/default.nix
+++ hosts/desktop-pc/default.nix
@@ -9,9 +9,13 @@
     ../../modules/system/core.nix
     ../../modules/system/netbird.nix
     
-    # ../../modules/desktop/hyprland.nix
-     ../../modules/desktop/mangowm.nix 
-    
+    ../../modules/desktop/hyprland.nix
+    #../../modules/desktop/mangowm.nix 
+    ../../modules/desktop/hyprshot.nix
+    ../../modules/desktop/waybar.nix
+    ../../modules/desktop/theme.nix
+    ../../modules/desktop/launcher.nix
+
     ../../modules/programs/nvim.nix
     ../../modules/programs/terminal.nix
     ../../modules/programs/librewolf.nix
@@ -21,11 +25,40 @@
 
   networking.hostName = "desktop-pc";
 
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    device = "nodev";
-    useOSProber = true;
+#  boot.loader.grub = {
+#    enable = true;
+#    efiSupport = true;
+#    device = "nodev";
+#    useOSProber = true;
+#  };
+#  boot.loader.efi.canTouchEfiVariables = true;
+  
+  lawford.system.core.enable = true;
+  lawford.desktop.hyprshot.enable = true;
+  lawford.desktop.hyprland.enable = true;
+  lawford.desktop.waybar.enable = true;
+  lawford.desktop.theme.enable = true;
+  lawford.desktop.launcher.enable = true;
+  
+  lawford.programs.terminal.enable = true;
+  lawford.programs.neovim.enable = true;
+  lawford.programs.librewolf.enable = true;
+  lawford.programs.ssh.enable = true;
+
+  lawford.services.netbird.enable = true;
+  lawford.virtualisation.docker.enable = true;
+
+  users.users.lawford = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" "docker" ];
+    shell = pkgs.zsh;
   };
-  boot.loader.efi.canTouchEfiVariables = true;
+
+  environment.systemPackages = with pkgs; [
+    mumble
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "23.11";
+  home-manager.users.lawford.home.stateVersion = "23.11";
 }
