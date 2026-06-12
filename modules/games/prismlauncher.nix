9cb9f4b57f23ef2f29360d715dba4d109bfe5e05
Ilya <alexander35480@gmail.com>
2026-06-13 03:48:33 +0500

    update prismalauncher

--- modules/games/prismlauncher.nix
+++ modules/games/prismlauncher.nix
@@ -2,17 +2,31 @@
 
 let
   cfg = config.lawford.games.prismlauncher;
-  
-  prismlauncher-9_1 = pkgs.prismlauncher.overrideAttrs (old: rec {
+
+  libnbtplusplus = pkgs.fetchFromGitHub {
+    owner = "PrismLauncher";
+    repo = "libnbtplusplus";
+    rev = "23b955121b8217c1c348a9ed2483167a6f3ff4ad";
+    hash = "sha256-BMgylHeKZd4FQKYoAxFSt+xAH+O1DBtAxdbQ5mJFE98=";
+  };
+
+  prismlauncher-unwrapped-9_1 = pkgs.prismlauncher-unwrapped.overrideAttrs (old: {
     version = "9.1";
     src = pkgs.fetchFromGitHub {
       owner = "PrismLauncher";
       repo = "PrismLauncher";
-      rev = version;
-      hash = "sha256-Vy3TnmQhYfqbLCM4sgUHFdj6xD8N8BQBhzTD/pjNInk=";
-      fetchSubmodules = true;
+      rev = "9.1";
+      hash = "sha256-WbolqvqudZq520p6pF86mFDMorpaY6E6oKCnd9f33us=";
     };
+    postUnpack = ''
+      rm -rf source/libraries/libnbtplusplus
+      ln -s ${libnbtplusplus} source/libraries/libnbtplusplus
+    '';
   });
+
+  prismlauncher-9_1 = pkgs.prismlauncher.override {
+    prismlauncher-unwrapped = prismlauncher-unwrapped-9_1;
+  };
 in
 {
   options.lawford.games.prismlauncher = {
