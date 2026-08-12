87059f84498d4676f311bcd0feb3c4d86144e801
ilya <ilya@miraidev.kz>
2026-08-13 01:05:13 +0500

    updated dnsmasq

--- modules/system/dnsmasq.nix
+++ modules/system/dnsmasq.nix
@@ -6,10 +6,8 @@ in
   options.lawford.system.dnsmasq = {
     enable = lib.mkEnableOption "Enable local DNS caching with dnsmasq";
   };
-
   config = lib.mkIf cfg.enable {
     services.resolved.enable = false;
-
     services.dnsmasq = {
       enable = true;
       resolveLocalQueries = true;
@@ -17,26 +15,24 @@ in
       settings = {
         listen-address = [ "127.0.0.1" ];
         bind-interfaces = true;
-
         server = [
           "94.140.14.14"
           "94.140.15.15"
           "1.1.1.1"
           "1.0.0.1"
         ];
-        
-        #cache-size = 1000;
-        #min-cache-ttl = 500;
+
+        cache-size = 10000;
+        min-cache-ttl = 500;
         #max-cache-ttl = 86400;
-        
+
+        dns-forward-max = 1000;
+
         domain-needed = true;
         bogus-priv = true;
         localise-queries = true;
       };
     };
-
     networking.nameservers = [ "127.0.0.1" ];
-    
-    networking.networkmanager.dns = "dnsmasq";
   };
 }
